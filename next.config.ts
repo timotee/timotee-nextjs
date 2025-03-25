dbfde04450b6c32f0aee63c8ea46863f7424f1eb
Tim Hofman <tim.hofman@timotee.com>
2025-03-25 10:47:04 +0100

    Solve serving on a path later

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
-// @ts-check
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
-const NextConfig = (phase: string) : NextConfig => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    basePath: isDev ? undefined : '/timotee-nextjs',
-    assetPrefix: isDev ? undefined : 'https://timotee.github.io/timotee-nextjs/',
-  }
-  return nextConfig
-}
-
-export default NextConfig;
+export default nextConfig;
